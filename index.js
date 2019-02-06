f0b79beb35eb9ed68a4ac52bf706f0d94d528fe0
Dibesh Shrestha <Dibesh.Shrestha@allscripts.com>
2019-02-06 15:13:44 -0500

    socket.io and express version updated

--- index.js
+++ index.js
@@ -14,11 +14,13 @@ app.use(express.static('public'));
 var io = socket(server);
 io.on('connection', (socket) => {
 
-    console.log('made socket connection', socket.id);
+    console.log('\nmade socket connection :', socket.id);
+    console.info('made socket connection hanshake url :', socket.handshake.url);
+    console.log('made socket connection  clientsCount :',  socket.conn.server.clientsCount);
 
     // Handle chat event
     socket.on('chat', function(data){
-        // console.log(data);
+        console.log(data);
         io.sockets.emit('chat', data);
     });
 
@@ -26,5 +28,13 @@ io.on('connection', (socket) => {
     socket.on('typing', function(data){
         socket.broadcast.emit('typing', data);
     });
+	
+	 socket.on('error', function (err_msg) {
+		console.log('Connection Error: ',  err_msg);
+	});
+
+	socket.on('disconnect', function(msg){
+		console.log('user disconnected :',  msg);
+	});
 
 });
